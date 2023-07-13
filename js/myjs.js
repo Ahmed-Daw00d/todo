b4b131ea1eea30b64931e2f49631632cfcec5328
Ahmed-Daw00d <adawood331@gmail.com>
2023-07-13 15:22:04 +0200

    first commit

---
+++ js/myjs.js
@@ -0,0 +1,15 @@
+var text;
+function add() {
+  text = document.querySelector("input[type=text]");
+  if (text.value!= "") {
+    var textLi = document.createTextNode(text.value);
+    var el = document.createElement("li");
+    el.appendChild(textLi);
+    var list = document.getElementById("list");
+
+    list.appendChild(el);
+    text.value = "";
+  } else {
+    alert("شكلك جاي تهزر🤔🤨اكتب حاجه ياعم😁");
+  }
+}
