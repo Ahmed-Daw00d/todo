8a429c9b75a81c24c5f5758ac062f8fb952f490c
Ahmed-Daw00d <adawood331@gmail.com>
2023-07-14 19:21:46 +0200

    1.3

--- js/myjs.js
+++ js/myjs.js
@@ -1,16 +1,16 @@
 var text;
 function add() {
   text = document.querySelector("input[type=text]");
-  if (text.value!= "") {
+  if (text.value != "") {
     var textLi = document.createTextNode(text.value);
     var el = document.createElement("li");
     el.appendChild(textLi);
     var list = document.getElementById("list");
-
+    document.querySelector("button").style.backgroundColor = "rgb(253,125,0)";
     list.appendChild(el);
     text.value = "";
   } else {
     alert("شكلك جاي تهزر🤔🤨اكتب حاجه ياعم😁");
+    document.querySelector("button").style.backgroundColor = "red";
   }
 }
-
