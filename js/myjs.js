d5e91e4f6b7ab65f585a68cbaeab653b35ef2451
Ahmed-Daw00d <adawood331@gmail.com>
2023-07-18 18:59:08 +0200

    4

--- js/myjs.js
+++ js/myjs.js
@@ -15,6 +15,7 @@ function add() {
       var textLi = document.createTextNode(toDo[i]);
       var el = document.createElement("li");
       el.appendChild(textLi);
+      
     }
     //add li in ul
     listUl.appendChild(el);
@@ -29,22 +30,53 @@ function add() {
     btn.classList.add("delete");
     //  add button complete
     var btnCom = document.createElement("button");
-    btnCom.innerHTML = '<i class="fa-solid fa-thumbs-up " style="color: #0f0;"></i>';
+    btnCom.innerHTML =
+      '<i class="fa-solid fa-thumbs-up " style="color: #0f0;"></i>';
     btnCom.classList.add("complete");
+    //  add button edit
+    var btnEdit = document.createElement("button");
+    btnEdit.innerHTML =
+      '<i class="fa-solid fa-pencil" style="color: #ebedef;"></i>';
+    btnEdit.classList.add("Edit");
     //for to add button to li
     for (let i = 0; i < li.length; i++) {
       li[i].appendChild(btn);
       li[i].appendChild(btnCom);
+      li[i].appendChild(btnEdit);
     }
     // function to delete li
     btn.addEventListener("click", () => {
-      listUl.removeChild(el);
+      if(confirm("are you sure about that 🤨 ?")){listUl.removeChild(el);}
+      
     });
 
     //function complete
-    btnCom.addEventListener("click",()=>{
-     el.classList.toggle("liComplete");
-    })
+    btnCom.addEventListener("click", () => {
+      el.classList.toggle("liComplete");
+    });
+    //function Edit
+    var EditInput;
+
+    var test = false;
+    btnEdit.addEventListener("click", () => {
+      if (test == false) {
+        EditInput = document.createElement("textarea");
+        el.appendChild(document.createElement("br"))
+        el.appendChild(EditInput);
+         
+        EditInput.value = el.firstChild.nodeValue;
+        test = true;
+      } else {
+        if (confirm("are you sure?🤔")) {
+          el.firstChild.nodeValue = EditInput.value;
+          el.removeChild(EditInput);
+        } else {
+          el.removeChild(EditInput);
+        }
+
+        test = false;
+      }
+    });
   } else {
     alert("شكلك جاي تهزر🤔🤨اكتب حاجه ياعم😁");
     document.querySelector("button").style.backgroundColor = "red";
